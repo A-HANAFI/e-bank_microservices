1504c2a8d546ba8a5fdda1cd8b6849ffe7e4ce7c
Ahmed HANAFI <a7med.connect@gmail.com>
2024-02-15 12:00:12 +0100

    configurations managent in microservices

--- accounts/src/main/java/net/ahmed/bank/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/net/ahmed/bank/accounts/dto/AccountsContactInfoDto.java
@@ -1,10 +1,16 @@
 package net.ahmed.bank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "contacts")
-public record AccountsContactInfoDto(String message, Map<String ,String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String ,String> contactDetails;
+    private List<String> onCallSupport;
 }
